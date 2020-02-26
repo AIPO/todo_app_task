2bce0a63d8c054f535abbde29d0f77991a35b667
AIPO <p.vaitonis@gmail.com>
2020-02-26 14:33:41 +0200

    updated store response

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreTask;
+use App\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function fetchAll()
+    {
+        return Task::orderBy('created_date', 'asc')->get();
+    }
+
+    public function store(StoreTask $task)
+    {
+        if (!$task->validated()) {
+            Task::create(['title' => $task->title]);
+        }
+
+        return Task::orderBy('created_date', 'asc')->get();
+    }
+
+    public function complete($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->completed = 'true';
+        $task->save();
+    }
+
+    public function edit(StoreTask $task)
+    {
+        if ($task->validated()) {
+            $currentTask = Task::findOrFail($task->id);
+            $currentTask->title = $task->title;
+            $currentTask->save();
+        }
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+    }
 }
